539273209212f1213eed8024f7d695e470042329
FahadMehmood123 <faddy.xl123@gmail.com>
2026-01-08 20:17:07 +0500

    Drag and Drop Fucntionality

---
+++ Tests/Drag_Drop.spec.js
@@ -0,0 +1,64 @@
+import {test,expect} from '@playwright/test';
+
+test(`Drag and drop functionality`,async({page})=>{
+
+    await page.goto('https://demoqa.com/droppable');    
+    await page.dragAndDrop('#draggable','.simple-drop-container>#droppable');  // source , target
+    expect(page.locator(".simple-drop-container>#droppable")).toHaveCSS('background-color','rgb(70, 130, 180)')
+    expect(page.locator(".simple-drop-container>#droppable")).toContainText('Dropped!');
+})
+
+
+test(`Drag and drop Revert Dragable`,async({page})=>{
+
+    await page.goto('https://demoqa.com/droppable');
+    await page.locator('#droppableExample-tab-revertable').click();
+    const currentPosition=await page.locator("#revertable").evaluate((el)=>{
+        const style = window.getComputedStyle(el);
+        return {
+            top: style.top,
+            left: style.left
+        };
+    });
+    //console.log(`Current position is x: ${currentPosition.left} and y: ${currentPosition.top}`);
+    await page.dragAndDrop('#revertable','.revertable-drop-container>#droppable');
+    await page.waitForTimeout(2000);
+    const newPostion=await page.locator("#revertable").evaluate((el)=>{
+        const style=window.getComputedStyle(el);
+        return{
+            top:style.top,
+            left:style.left
+        };
+    })
+    await page.waitForTimeout(2000);
+    expect(currentPosition.top).toBe(newPostion.top);
+    expect(currentPosition.left).toBe(newPostion.left);
+    
+    // await page.goto('https://demoqa.com/droppable');
+    // await page.locator('#droppableExample-tab-revertable').click();
+    // await page.waitForTimeout(2000);
+    // await page.dragAndDrop(".revertable-drop-container>div>#notRevertable",".simple-drop-container>#droppable");
+    // const currentPosition_notRevertable=await page.locator('#notRevertable').evaluate((el)=>{
+    //     const style=window.getComputedStyle(el);
+    //     return{
+    //         top:style.top,
+    //         left:style.left
+    //     }
+
+    // })
+
+    // await page.dragAndDrop('#notRevertable',`.revertable-drop-container>div:nth-child(1)`);
+    // await page.waitForTimeout(2000);
+    // const newPosition_notRevertable=await page.locator('#notRevertable').evaluate((el)=>{
+    //     const style=window.getComputedStyle(el);
+    //     return{
+    //         top:style.top,
+    //         left:style.left
+    //     }
+    // })
+    // expect(currentPosition_notRevertable.top).not.toBe(newPosition_notRevertable.top);
+    // expect(currentPosition_notRevertable.left).not.toBe(newPosition_notRevertable.left);
+    
+
+});
+
--- Tests/Read_Excel_file.spec.js
+++ Tests/Read_Excel_file.spec.js
@@ -5,18 +5,27 @@ import path from 'path';
 // install package 'xlsx' before running this test: npm install xlsx
 // refer to https://www.npmjs.com/package/xlsx for more details
 
+// steps to read excel file:
+//1: get the path of the excel file
+//2: read the excel file
+//3: get the sheet you want to read
+//4: convert sheet to json format
+//5: use the json data in your test
+
+
 test('Read Excel file and verify content', async ({}) => {
-  // Define the path to the Excel file
+  // 1: Define the path to the Excel file
     const excelFilePath = path.resolve(__dirname, '../Tests/file_example_XLSX_10.xlsx');
-    // Read the Excel file
+    // 2: Read the Excel file
     const workbook = XLSX.readFile(excelFilePath);
-    // Get the first sheet name
+    // 3: Get the first sheet name
     const firstSheetName = workbook.SheetNames[0];
-    // Get the first sheet
+    // 4: Get the first sheet
     const worksheet = workbook.Sheets[firstSheetName];
-    // Convert sheet to JSON
+    // 5:  Convert sheet to JSON
     const jsonData = XLSX.utils.sheet_to_json(worksheet);
     // Log the JSON data to console
-    console.log(jsonData[0]);
+    console.log(jsonData[0]); // first row data
+
 
 });
