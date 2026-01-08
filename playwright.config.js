6a7262f7033520e8f22e62388a08c2530779998c
FahadMehmood123 <faddy.xl123@gmail.com>
2026-01-08 11:48:38 +0500

    Modified config file

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   timeout: 50* 1000,
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
   // retries: 1,
